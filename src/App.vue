444060f72702776e78318e54a3bc11e9f522de61
Jacob Lin <jacoblindev@gmail.com>
2022-03-07 18:13:02 +0800

    style: add logo & base style

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,18 @@ import { RouterView } from "vue-router";
   padding: 0;
   box-sizing: border-box;
 }
+body {
+  min-height: 100vh;
+  color: #c8b6ff;
+  background: #181818;
+  transition: color 0.5s, background-color 0.5s;
+  line-height: 1.6;
+  font-family: Inter, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif;
+  font-size: 16px;
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>
