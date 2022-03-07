8d0b1e27eea5674a27dbd4f2b901e1a56c8088a3
Jacob Lin <jacoblindev@gmail.com>
2022-03-07 18:12:29 +0800

    feat: add FontAwesomeIcon packages

--- src/main.js
+++ src/main.js
@@ -4,9 +4,20 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import {
+  faGithubSquare,
+  faInstagramSquare,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faGithubSquare, faInstagramSquare);
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
