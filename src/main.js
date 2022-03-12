047f5b08570b7617e211a0639e323f8b30bf07bb
Jacob Lin <jacoblindev@gmail.com>
2022-03-12 23:18:10 +0800

    feat: edit the home page's hero section

--- src/main.js
+++ src/main.js
@@ -6,14 +6,11 @@ import router from "./router";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import {
-  faGithubSquare,
-  faInstagramSquare,
-} from "@fortawesome/free-brands-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import {} from "@fortawesome/free-regular-svg-icons";
 import { faExternalLinkSquare } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faGithubSquare, faInstagramSquare, faExternalLinkSquare);
+library.add(faGithub, faExternalLinkSquare);
 
 const app = createApp(App);
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,16 +5,32 @@ import ChallengeCard from "../components/ChallengeCard.vue";
 
 const challengeStore = useChallengeStore();
 
-const searchTerm = ref("");
+const searchByName = ref("");
+const filterByLevel = ref("");
 
 const filteredChallenges = computed(() => {
-  if (searchTerm.value !== "") {
-    return challengeStore.challenges.filter((e) =>
-      e.title.toLowerCase().includes(searchTerm.value.toLowerCase())
-    );
-  } else {
-    return challengeStore.challenges;
+  let filteredArr = challengeStore.challenges;
+  if (filterByLevel.value !== "") {
+    switch (filterByLevel.value) {
+      case "NEWBIE":
+        filteredArr = filteredArr.filter((e) => e.level.includes("NEWBIE"));
+        break;
+      case "JUNIOR":
+        filteredArr = filteredArr.filter((e) => e.level.includes("JUNIOR"));
+        break;
+      default:
+        filteredArr = filteredArr.filter((e) =>
+          e.level.includes(filterByLevel.value)
+        );
+        break;
+    }
   }
+  if (searchByName.value !== "") {
+    filteredArr = filteredArr.filter((e) =>
+      e.title.toLowerCase().includes(searchByName.value.toLowerCase())
+    );
+  }
+  return filteredArr;
 });
 </script>
 
@@ -26,23 +42,49 @@ const filteredChallenges = computed(() => {
   </nav>
   <main>
     <section id="hero-section">
-      <h2>Welcome to my Frontend Mentor's challenge solutions with Vue 3!</h2>
-      <q>
-        Frontend Mentor challenges help you improve your coding skills by
-        building realistic projects.
-      </q>
-      <p>
-        My goal is to use these challenges to practice my Vue 3 skills & vanilla
-        CSS skills.
-      </p>
+      <section id="hero-welcome">
+        <h2>Welcome to my Frontend Mentor's challenge solutions with Vue 3!</h2>
+        <q>
+          Frontend Mentor challenges help you improve your coding skills by
+          building realistic projects.
+        </q>
+        <h4>
+          My goal is to use these challenges to practice my Vue 3 & CSS skills.
+        </h4>
+        <div>
+          <a
+            target="_blank"
+            href="https://github.com/jacoblindev/vue-fm-challenges"
+            rel="noopener noreferrer"
+          >
+            <font-awesome-icon :icon="['fab', 'github']" size="lg" />
+            | REPOSITORY
+          </a>
+        </div>
+      </section>
+      <section id="hero-img">
+        <img src="../assets/undraw_experience_design.svg" alt="undraw" />
+      </section>
     </section>
     <nav id="filter-navbar">
-      <h3>Challengs</h3>
+      <span>Challengs</span>
       <span>
         <input
           type="text"
-          v-model="searchTerm"
-          placeholder="Search by name..."
+          v-model="filterByLevel"
+          list="filter-by-level"
+          placeholder="Filter by level"
+        />
+        <datalist id="filter-by-level">
+          <option>NEWBIE</option>
+          <option>JUNIOR</option>
+        </datalist>
+      </span>
+      <span>
+        <input
+          type="text"
+          v-model="searchByName"
+          placeholder="Search by name"
         />
       </span>
     </nav>
@@ -50,6 +92,9 @@ const filteredChallenges = computed(() => {
       <template v-for="(challenge, index) in filteredChallenges" :key="index">
         <ChallengeCard v-bind:challenge="challenge" />
       </template>
+      <div id="empty-msg" v-if="filteredChallenges.length === 0">
+        Nothing matched..., please filter or search again! Cheers~!
+      </div>
     </section>
   </main>
   <footer>
@@ -70,7 +115,7 @@ const filteredChallenges = computed(() => {
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #181818;
+  background-color: #181818e8;
   position: fixed;
   top: 0;
   left: 0;
@@ -79,60 +124,104 @@ const filteredChallenges = computed(() => {
   z-index: 99;
 }
 #home-navbar > a {
-  text-decoration: none;
   margin: 0.5rem 0;
-  padding: 0.2rem 3rem;
+  padding: 0.25rem 3rem;
   border-left: 1px solid #6247aa;
   border-right: 1px solid #6247aa;
 }
 #home-navbar > a > img {
-  height: 6vh;
+  height: 3rem;
   vertical-align: middle;
 }
 main {
-  min-height: 100vh;
-  padding: 6rem 2rem 6rem;
-  text-align: center;
+  padding: 5rem 0 2rem;
 }
 main section {
   display: flex;
   gap: 1.5rem;
-  flex-wrap: wrap;
   justify-content: center;
 }
 #hero-section {
-  padding: 10rem 5rem;
+  align-items: center;
+  padding: 3rem 5rem;
+}
+#hero-welcome {
+  width: 50%;
   flex-direction: column;
+}
+#hero-welcome > h2 {
+  font-size: 2.6rem;
+}
+#hero-welcome > q {
+  background-color: #6247aa;
+  border-left: 5px solid #c8b6ff;
+  border-right: 5px solid #c8b6ff;
+  padding: 0.8rem;
+  font-weight: bold;
+}
+#hero-welcome > div > a {
+  text-decoration: none;
+  color: #c8b6ff;
+  text-align: center;
+  padding: 0.5rem;
+  border: #c8b6ff 1px solid;
+}
+#hero-welcome > div > a:hover {
+  background-color: #6247aa;
+}
+#hero-img {
+  width: 50%;
+}
+#hero-img > img {
+  width: 70vh;
 }
 #filter-navbar {
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   padding: 0 7rem;
   margin: 1.5rem 0;
   border-top: 1px solid #6247aa;
   border-bottom: 1px solid #6247aa;
+  position: sticky;
+  top: 4.5rem;
+  z-index: 88;
+  background-color: #181818;
 }
-#filter-navbar > h3,
 #filter-navbar > span {
-  padding: 0.5rem 2rem;
-  border-left: 1px solid #6247aa;
+  padding: 0.5rem 1.2rem;
   border-right: 1px solid #6247aa;
+  font-weight: bold;
+}
+#filter-navbar > span:first-child {
+  border-left: 1px solid #6247aa;
 }
 #filter-navbar input {
   background-color: transparent;
   border: none;
   vertical-align: middle;
-  padding: 0.4rem 2rem;
+  padding: 0.2rem 0.8rem;
   border-radius: 50px;
   outline: none;
   caret-color: #6247aa;
   outline: 1px dashed #6247aa;
   color: #c8b6ff;
   font-size: 16px;
+  vertical-align: bottom;
+  text-align: center;
 }
 #filter-navbar input:focus {
   outline: 2px solid #c8b6ff;
+}
+#cards-section {
+  text-align: center;
+  flex-wrap: wrap;
+}
+#cards-section #empty-msg {
+  min-height: 45vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 footer {
   font-weight: 300;
@@ -149,19 +238,34 @@ footer a {
   text-decoration: none;
   font-weight: bold;
 }
-@media screen and (max-width: 799px) {
+@media screen and (max-width: 1200px) {
   #hero-section {
-    padding: 3.5rem 1.5rem;
+    flex-direction: column-reverse;
+    padding: 2rem;
   }
+  #hero-welcome {
+    width: 100%;
+    text-align: center;
+  }
+  #hero-welcome > h2 {
+    font-size: 1.6rem;
+  }
+  #hero-welcome > a {
+    width: 100%;
+  }
+  #hero-img {
+    width: 100%;
+  }
+  #hero-img > img {
+    width: 40vh;
+  }
+}
+@media screen and (max-width: 800px) {
   #filter-navbar {
     flex-direction: column;
     padding: 0;
   }
-  #filter-navbar > h3 {
-    padding: 0.5rem 2rem;
-    border: none;
-  }
-  #filter-navbar > span {
+  #filter-navbar > span:not(:first-child) {
     display: none;
   }
   footer {
